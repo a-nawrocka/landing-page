50930ecf8a7dcbcd19e9282182794eada3f37ea7
Anna Nawrocka <anna.nawrocka@wrbm.com>
2022-08-11 17:25:52 +0100

    Style navigation and general flow

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+	navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible")); 
+	primaryNav.toggleAttribute("data-visible");	
+	primaryHeader.toggleAttribute("data-overlay");	
+  });
+
+
+
+
+  
